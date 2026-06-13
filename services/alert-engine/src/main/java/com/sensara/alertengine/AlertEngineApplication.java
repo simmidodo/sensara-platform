74bed0f2da9c7c165511e2332255ec42752de98f
7777simi <7777simi@gmail.com>
2026-06-13 23:25:11 +0100

    Add alert engine Spring Boot skeleton

---
+++ services/alert-engine/src/main/java/com/sensara/alertengine/AlertEngineApplication.java
@@ -0,0 +1,11 @@
+package com.sensara.alertengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertEngineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AlertEngineApplication.class, args);
+    }
+}
